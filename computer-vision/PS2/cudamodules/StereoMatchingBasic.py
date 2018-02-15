59836cc39f45838d11d9deb82cbc86496ead4b31
thibautBoissin <thibaut@homiwoo.com>
2018-02-15 23:22:48 +0100

    PS2 cross correlation working in python

--- computer-vision/PS2/cudamodules/StereoMatchingBasic.py
+++ computer-vision/PS2/cudamodules/StereoMatchingBasic.py
@@ -31,17 +31,26 @@ int64_t aij;
 int64_t bij;
 int64_t sa = 0;
 int64_t sb = 0;
-int64_t d = 0;
+float d = 0.0;
+float div = 0.0;
+// split the computation in two steps to avoid overflow
 for( i=-wdiam; i<=wdiam; i++){
     for( j=-wdiam; j<=wdiam; j++){
         aij = a[(ay+i)+((ax+j)*imy)];
         bij = b[(bx+i)+((ax+j)*imy)];
         sa = sa + (aij*aij);
         sb = sb + (bij*bij);
-        d  = d  + (aij*bij);
     }
 }
-scores[tix] = ((float)d) / sqrtf((float)sa*sb);
+div = sqrtf((float)sa)*sqrtf((float)sb);
+for( i=-wdiam; i<=wdiam; i++){
+    for( j=-wdiam; j<=wdiam; j++){
+        aij = a[(ay+i)+((ax+j)*imy)];
+        bij = b[(bx+i)+((ax+j)*imy)];
+        d  = d  + ((aij*bij) / div);
+    }
+}
+scores[tix] =  d;
 """
 
 
@@ -71,7 +80,7 @@ def testCuda():
     print(a)
 
 
-def stereo_matching_basic(img1, img2, ws, kernel=KERNEL_CSS):
+def stereo_matching_basic(img1, img2, ws, kernel=KERNEL_CSS, dmin=30, dmax=120):
     (imx, imy) = img1.shape
     assert (img2.shape == (imx, imy))
     # block x and block y => x and y position of the mask on image a
@@ -82,9 +91,9 @@ def stereo_matching_basic(img1, img2, ws, kernel=KERNEL_CSS):
             const int64_t bix = blockIdx.x;
             const int64_t biy = blockIdx.y;
             const int64_t tix = threadIdx.x;
-            const int64_t ws = """+str(ws)+""";
-            const int64_t imx = """+str(imx)+""";
-            const int64_t imy = """+str(imy)+""";
+            const int64_t ws = """ + str(ws) + """;
+            const int64_t imx = """ + str(imx) + """;
+            const int64_t imy = """ + str(imy) + """;
             const int64_t wdiam = (ws-1)/2;
             const int64_t resdx = imx-ws+1;
             const int64_t resdy = imy-ws+1;
@@ -95,15 +104,16 @@ def stereo_matching_basic(img1, img2, ws, kernel=KERNEL_CSS):
             register int64_t j;
             register int64_t k;
             
-            """+kernel+"""
+            """ + kernel + """
             
             __syncthreads();
             if (tix == 0){
-                for (k=0; k<resdx; k++){
+            """+ str('for (k=0; k<resdx; k++){' if (dmin >= 0) else 'for (k=(resdx-1); k>=0; k--){')
+                +"""
                     d = scores[k];
                     if ((k==0)||(d <= best)){
                         best = d;
-                        if (((biy - k)>=0)&&((biy - k)<200)){ 
+                        if (((biy - k)>=""" + str(dmin) + """)&&((biy - k)<=""" + str(dmax) + """)){ 
                             result[biy + (bix*resdy)] = biy - k;
                         }
                     }
@@ -120,37 +130,39 @@ def stereo_matching_basic(img1, img2, ws, kernel=KERNEL_CSS):
     cuda.memcpy_htod(img1_gpu, img1)
     cuda.memcpy_htod(img2_gpu, img2)
 
-    matches = numpy.zeros((imx - ws+1, imy - ws+1), dtype=numpy.int64)
+    matches = numpy.full((imx - ws + 1, imy - ws + 1), min(abs(dmax), abs(dmin)), dtype=numpy.int64)
     matches_gpu = cuda.mem_alloc(matches.nbytes)
     cuda.memcpy_htod(matches_gpu, matches)
 
     func(img1_gpu, img2_gpu, matches_gpu, block=(imx - ws + 1, 1, 1),
          grid=(imx - ws + 1, imy - ws + 1))
     cuda.memcpy_dtoh(matches, matches_gpu)
+    if dmin < 0:
+        matches = matches * -1
     return matches
 
 
 if __name__ == "__main__":
-    # pylab.imshow(stereo_matching_basic(
-    # cv2.cvtColor(cv2.imread('Data/leftTest.png'), cv2.COLOR_BGR2GRAY),
-    # cv2.cvtColor(cv2.imread('Data/rightTest.png'), cv2.COLOR_BGR2GRAY),
-    # 5), cmap=pylab.gray())
-    # pylab.show()
-
     pylab.imshow(stereo_matching_basic(
-    cv2.cvtColor(cv2.imread('Data/leftTest.png'), cv2.COLOR_BGR2GRAY),
-    cv2.cvtColor(cv2.imread('Data/rightTest.png'), cv2.COLOR_BGR2GRAY),
-    5, kernel=KERNEL_CROSS_CORR), cmap=pylab.gray())
+        cv2.cvtColor(cv2.imread('Data/leftTest.png'), cv2.COLOR_BGR2GRAY),
+        cv2.cvtColor(cv2.imread('Data/rightTest.png'), cv2.COLOR_BGR2GRAY),
+        5), cmap=pylab.gray())
     pylab.show()
 
-    # pylab.imshow(stereo_matching_basic(
-    # cv2.cvtColor(cv2.imread("Data/proj2-pair1-L.png"), cv2.COLOR_BGR2GRAY),
-    # cv2.cvtColor(cv2.imread("Data/proj2-pair1-R.png"), cv2.COLOR_BGR2GRAY),
-    # 5), cmap=pylab.gray())
-    # pylab.show()
+    pylab.imshow(stereo_matching_basic(
+        cv2.cvtColor(cv2.imread('Data/leftTest.png'), cv2.COLOR_BGR2GRAY),
+        cv2.cvtColor(cv2.imread('Data/rightTest.png'), cv2.COLOR_BGR2GRAY),
+        5, kernel=KERNEL_CROSS_CORR), cmap=pylab.gray())
+    pylab.show()
 
     pylab.imshow(stereo_matching_basic(
-    cv2.cvtColor(cv2.imread("Data/proj2-pair1-L.png"), cv2.COLOR_BGR2GRAY),
-    cv2.cvtColor(cv2.imread("Data/proj2-pair1-R.png"), cv2.COLOR_BGR2GRAY),
-    5, kernel=KERNEL_CROSS_CORR), cmap=pylab.gray())
+        cv2.cvtColor(cv2.imread("Data/proj2-pair1-L.png"), cv2.COLOR_BGR2GRAY),
+        cv2.cvtColor(cv2.imread("Data/proj2-pair1-R.png"), cv2.COLOR_BGR2GRAY),
+        10), cmap=pylab.gray())
+    pylab.show()
+
+    pylab.imshow(stereo_matching_basic(
+        cv2.cvtColor(cv2.imread("Data/proj2-pair1-L.png"), cv2.COLOR_BGR2GRAY),
+        cv2.cvtColor(cv2.imread("Data/proj2-pair1-R.png"), cv2.COLOR_BGR2GRAY),
+        5, kernel=KERNEL_CROSS_CORR), cmap=pylab.gray())
     pylab.show()
