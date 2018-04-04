7027b9b94cb6fc1df10b0fd3b5c744f64b197f61
thibautBoissin <thibaut@homiwoo.com>
2018-04-04 16:55:07 +0200

    PS5 hierarchical LK working, border still needs to be managed

--- computer-vision/PS4/tools/sobel.py
+++ computer-vision/PS4/tools/sobel.py
@@ -3,39 +3,8 @@ import numpy as np
 
 
 def compute_gradients(img1, blursize=1, kersize=3):
-    # kernel_smooth = np.vectorize(lambda x: x * 0.1)(np.array(
-    #     [
-    #         [1, 2, 1],
-    #         [2, 4, 2],
-    #         [1, 2, 1]
-    #     ]
-    # )
-    # )
-    img1 = cv2.GaussianBlur(src=img1,
-                            ksize=(4*blursize+1, 4*blursize+1),
-                            sigmaX=blursize, sigmaY=blursize,
-                            borderType=cv2.BORDER_REFLECT)
-    # img1 = cv2.filter2D(img1, -1, kernel_smooth, borderType=cv2.BORDER_CONSTANT)
-
-    # kernel = np.vectorize(lambda x: x * 0.125)(np.array(
-    #     [
-    #         [-1, 0, 1],
-    #         [-2, 0, 2],
-    #         [-1, 0, 1]
-    #     ]
-    # ))
-    # kernel_h = np.vectorize(lambda x: x * 0.125)(np.array(
-    #     [
-    #         [-1, -2, -1],
-    #         [0, 0, 0],
-    #         [1, 2, 1]
-    #     ]
-    # ))
-
     img_x = cv2.Sobel(img1, cv2.CV_64F, 1, 0, ksize=kersize)
-    # cv2.filter2D(img1, -1, kernel, borderType=cv2.BORDER_REFLECT)
     img_y = cv2.Sobel(img1, cv2.CV_64F, 0, 1, ksize=kersize)
-    # cv2.filter2D(img1, -1, kernel_h, borderType=cv2.BORDER_REFLECT)
     return img_x, img_y
 
 
--- computer-vision/PS5/tools/lucas_kanade.py
+++ computer-vision/PS5/tools/lucas_kanade.py
@@ -1,11 +1,19 @@
 import cv2
-import pylab
 import numpy as np
+import pylab
 from numba import jit, cuda
-from tools import sobel
+from tools import sobel, laplacian_pyramid
 
 
 def compute_lk(grad_x, grad_y, grad_t, wd):
+    """
+    compute the LK optical flow without using CUDA
+    :param grad_x: x gradient of image
+    :param grad_y: y gradient of image
+    :param grad_t: temporal gradient of image
+    :param wd: the window size
+    :return: U and V corresponding to optical flow
+    """
     U = np.zeros(grad_x.shape, dtype=float)
     V = np.zeros(grad_x.shape, dtype=float)
     R = np.zeros_like(grad_x)  # todo: set the correct size for r
@@ -35,8 +43,15 @@ def compute_lk(grad_x, grad_y, grad_t, wd):
     return U, V
 
 
-@cuda.jit  # (float[:, :], float[:, :], float[:, :], float[:, :], float[:, :], uint8)
+@cuda.jit
 def compute_lk_cuda(grad_x, grad_y, grad_t, U, V, wd):
+    """
+    CUDA kernel computing the optical flow
+    :param grad_x: x gradient of image
+    :param grad_y: y gradient of image
+    :param grad_t: temporal gradient of image
+    :param wd: the window size
+    """
     i = cuda.blockIdx.x + wd
     j = cuda.blockIdx.y + wd
     Ixx = 0.0
@@ -62,6 +77,14 @@ def compute_lk_cuda(grad_x, grad_y, grad_t, U, V, wd):
 
 @jit
 def wrapper_compute_lk_cuda(grad_x, grad_y, grad_t, wd):
+    """
+    wrapper to compute LK flow using CUDA
+    :param grad_x: x gradient of image
+    :param grad_y: y gradient of image
+    :param grad_t: temporal gradient of image
+    :param wd: the window size
+    :return: U and V corresponding to optical flow
+    """
     U = np.zeros(grad_x.shape, dtype=float)
     V = np.zeros(grad_x.shape, dtype=float)
     blockspergrid_x = grad_x.shape[0] - 2 * wd
@@ -79,6 +102,13 @@ def wrapper_compute_lk_cuda(grad_x, grad_y, grad_t, wd):
 
 
 def warp_img(img, U, V):
+    """
+    warp the image according to the U and V optical flow vectors
+    :param img: the image to warp
+    :param U:
+    :param V:
+    :return: the warped image
+    """
     (width, height) = U.shape
     mapU = np.zeros((width, height), dtype=np.float32)
     mapV = np.zeros((width, height), dtype=np.float32)
@@ -90,15 +120,71 @@ def warp_img(img, U, V):
                      borderMode=cv2.BORDER_CONSTANT)
 
 
+def LK_pyramid(img: dict, level, wd):
+    """
+    compute the hierachical LK optical flow, this function must be called trough wrapper_LK_pyramid
+    :param img: dictionnary containing various gradient used
+    :param level: the remaining number of steps
+    :param wd: the windows radius used in LK
+    :return: the U and V vector corresponding to the optical flow
+    """
+    # if we are on a leaf node
+    if level == 0:
+        # LK computation
+        return wrapper_compute_lk_cuda(img['grad_x'], img['grad_y'], img['grad_t'], int(wd))
+    # if we are not on a leaf node
+    img_red = {}
+    # step 1: reduce all gradients images
+    for key in img.keys():
+        img_red[key] = laplacian_pyramid.reduce(img[key])
+    # step 2: recursive call, get the result from the previous stages and expand it
+    (u, v) = LK_pyramid(img_red, level - 1, wd / 2)
+    u = laplacian_pyramid.expand(u)
+    v = laplacian_pyramid.expand(v)
+    # step 3 : warping
+    img_warped = {}
+    for key in img.keys():
+        img_warped[key] = warp_img(img[key], u, v)
+    # step 4 : current stage LK
+    (current_u, current_v) = wrapper_compute_lk_cuda(img_warped['grad_x'], img_warped['grad_y'], img_warped['grad_t'],
+                                                     int(wd))
+    # step 5 : add current result to the accumulator and return it
+    return current_u + u, current_v + v
+
+
+def wrapper_LK_pyramid(img0, img1, depth, wd):
+    """
+    compute the hierarchical Lk optic flow of an image using CUDA
+    :param img0: the image at t
+    :param img1: the image at t+1
+    :param depth: the depth of the pyramid
+    :param wd: the size of the window
+    :return: the U and V corresponding to optical flow
+    """
+    grad_t = img1 - img0
+    grad_t = grad_t.astype(float)
+    grad_x, grad_y = sobel.compute_gradients(img0)
+    img = {
+        'grad_x': grad_x,
+        'grad_y': grad_y,
+        'grad_t': grad_t,
+    }
+    return LK_pyramid(img, depth, wd)
+
+
 if __name__ == '__main__':
     im0 = cv2.imread("images/TestSeq/Shift0.png")[:, :, 0]
     im1 = cv2.imread("images/TestSeq/ShiftR2.png")[:, :, 0]
-    im0 = cv2.GaussianBlur(src=im0, ksize=(19, 7), sigmaX=10, sigmaY=10, borderType=cv2.BORDER_REFLECT)
-    im1 = cv2.GaussianBlur(src=im1, ksize=(19, 7), sigmaX=10, sigmaY=10, borderType=cv2.BORDER_REFLECT)
+    #im0 = cv2.GaussianBlur(src=im0, ksize=(19, 19), sigmaX=10, sigmaY=10, borderType=cv2.BORDER_REFLECT)
+    #im1 = cv2.GaussianBlur(src=im1, ksize=(19, 19), sigmaX=10, sigmaY=10, borderType=cv2.BORDER_REFLECT)
     grad_t = im1 - im0
     grad_t = grad_t.astype(float)
     grad_x, grad_y = sobel.compute_gradients(im0)
     U, V = wrapper_compute_lk_cuda(grad_x, grad_y, grad_t, 5)
     warped = warp_img(im0, U, V)
     cv2.imshow("warped image", np.hstack((im0, warped, im1)))
+    cv2.waitKey()
+    U2, V2 = wrapper_LK_pyramid(im0, im1, 5, 20)
+    warped = warp_img(im0, U2, V2)
+    cv2.imshow("warped image", warped)
     cv2.waitKey()
